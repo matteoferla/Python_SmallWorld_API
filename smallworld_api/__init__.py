39113d2be133a47eea7c76c6ccf60a68fa5b71bc
matteoferla <matteo.ferla@gmail.com>
2022-01-06 16:33:30 +0000

    :wrench: dropped number of explicit arguments to pointless get full points on codeclimate... :laughing:

--- smallworld_api/__init__.py
+++ smallworld_api/__init__.py
@@ -30,18 +30,31 @@ class SmallWorld(Extras):
     def search(self,
                smiles: str,
                db: str = 'REAL_DB_20Q2',
-               dist: int = 10,
-               start: int = 0,
-               length: int = 10,
-               draw: int = 10,
                **other_parameters) -> pd.DataFrame:
         """
         Given a smiles and a database return the table of results!
+
+        The optional arguments are:
+
+        * dist = 10 (atom difference distance threshhold)
+        * several in `.default_submission`...
+
+        The number of results given are controlled by:
+
+        * length = 10 (number of results)
+        * draw = 10 (pointless atm)
+        * start = 0
+
+        Which are passed onto `.get_results`.
 
         Calls ``submit_query`` and then ``get_results``.
         Returns a pandas dataframe of results.
         The dataframe is not rdkit modified yet.
         """
+        start = int(other_parameters['start']) if 'start' in other_parameters else 0
+        dist = int(other_parameters['dist']) if 'start' in other_parameters else 0
+        length = int(other_parameters['length']) if 'length' in other_parameters else 10
+        draw = int(other_parameters['draw']) if 'draw' in other_parameters else 10
         valids = {k: other_parameters[k] for k in self.valid_submit_keys if k in other_parameters}
         if db not in self.db_choices:
             warn(f'{db} is not a valid choice ({self.db_choices}).' + \
