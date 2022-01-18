0ea98e0ae706db14db445ef958ef8f4bd7818afc
matteoferla <matteo.ferla@gmail.com>
2022-01-18 18:09:01 +0000

    :pencil: prev edit is unusable changed to 'tolerate_NoMatchError'

--- smallworld_api/__init__.py
+++ smallworld_api/__init__.py
@@ -107,7 +107,7 @@ class SmallWorld(Searcher):  # Defaults -> Common -> Base -> Extras -> Searcher 
             # ## what to capture
             if 'tolerated_exceptions' in other_parameters:
                 tolerated_exceptions = other_parameters['tolerated_exceptions']
-            elif 'tolerate_no-matches' in other_parameters and other_parameters['tolerate_no-matches']:
+            elif 'tolerate_NoMatchError' in other_parameters and other_parameters['tolerate_NoMatchError']:
                 tolerated_exceptions = (NoMatchError,)
             else:
                 tolerated_exceptions = ()
