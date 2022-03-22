008eb909043a1d7da11ee461456a61e6c36fc6f0
matteoferla <matteo.ferla@gmail.com>
2022-03-22 23:42:23 +0000

    :bug: reverting as the module is def being read

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,7 @@
 #
 import os
 import sys
-print(os.path.abspath("../../"))
-print(os.listdir(os.path.abspath("../../")))
-#sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 
