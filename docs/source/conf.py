f419b771afbf3e3e0ff0fe0132d775a7698d7e65
matteoferla <matteo.ferla@gmail.com>
2022-03-22 23:40:24 +0000

    :bug: trying to see what happens if the sys path is not altered...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../../"))
+print(os.path.abspath("../../"))
+print(os.listdir(os.path.abspath("../../")))
+#sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 
